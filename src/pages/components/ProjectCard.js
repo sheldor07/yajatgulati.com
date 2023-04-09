0f5db958b381c4193dff6d4b0f795ecbb4062f7b
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-04-09 16:17:17 +0800

    accounted for empty tool array
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- src/pages/components/ProjectCard.js
+++ src/pages/components/ProjectCard.js
@@ -1,24 +1,55 @@
-import LinkArrow from "./LinkArrow"
-export default function ProjectCard ({name, description, githubLink, link,status,tools})
-{
-    return(
-        <div className="my-5 px-5 border-l-2 border-gray-400">
-            <h1 className="text-xl">{name}</h1>
-            <div className="flex flex-row gap-3">
-            <a className="mt-2 text-slate-600 flex flex-row items-center" target="__blank" href={link}>Live Preview<div className="w-4 h-4"> <LinkArrow/></div></a>
-            <a className="mt-2 text-slate-600 flex flex-row items-center" target="__blank" href={githubLink}>Github<div className="w-4 h-4"> <LinkArrow/></div></a>
-            </div>
-            <p className= "mt-5 text-gray-500">{description}</p>
-            <div className="text-lg flex flex-row gap-2 align-middle">tools: { tools?'':tools.map(
-                //write a function to make a small grey box with the tool name
-                (tool) => {
-                    return(
-                        <div className="text-sm bg-gray-300 rounded-md p-1 m-1">
-                            {tool}
-                        </div>
-                    )
-                }
-            )}</div>
-        </div>
-    )
+import LinkArrow from "./LinkArrow";
+export default function ProjectCard({
+  name,
+  description,
+  githubLink,
+  link,
+  status,
+  tools,
+}) {
+  return (
+    <div className="my-5 px-5 border-l-2 border-gray-400">
+      <h1 className="text-xl">{name}</h1>
+      <div className="flex flex-row gap-3">
+        <a
+          className="mt-2 text-slate-600 flex flex-row items-center"
+          target="__blank"
+          href={link}
+        >
+          Live Preview
+          <div className="w-4 h-4">
+            {" "}
+            <LinkArrow />
+          </div>
+        </a>
+        <a
+          className="mt-2 text-slate-600 flex flex-row items-center"
+          target="__blank"
+          href={githubLink}
+        >
+          Github
+          <div className="w-4 h-4">
+            {" "}
+            <LinkArrow />
+          </div>
+        </a>
+      </div>
+      <p className="mt-5 text-gray-500">{description}</p>
+      <div className="text-lg flex flex-row gap-2 align-middle">
+        tools:{" "}
+        {tools
+          ? tools.map(
+              //write a function to make a small grey box with the tool name
+              (tool) => {
+                return (
+                  <div className="text-sm bg-gray-300 rounded-md p-1 m-1">
+                    {tool}
+                  </div>
+                );
+              }
+            )
+          : ""}
+      </div>
+    </div>
+  );
 }
