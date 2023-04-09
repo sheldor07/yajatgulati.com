983f7b2abae04672f3b0e70cb108e9780e802ca2
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-04-10 03:12:54 +0800

    added seo tags
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,9 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        <title>Yajat Gulati</title>
+        <title>Yajat Gulati - Developer</title>
+  <meta name="description" content="18-year-old CS and Business student building software applications using new technologies. Currently open for exciting internships for the summer of 2023."/>
+  <meta name="keywords" content="software, developer, web development, AI, OpenAI, NextJS, TailwindCSS, Supabase, ReactJS, love letter, book club"></meta>
       </Head>
       <body>
         <Main />
