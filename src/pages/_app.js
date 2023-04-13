b31b7194f47509d2757561c63f811a70f0ef31c1
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-04-13 18:04:45 +0800

    pls work google analytics
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
+import Script from 'next/script'
 export default function App({ Component, pageProps }) {
 
   return (
@@ -11,7 +12,16 @@ export default function App({ Component, pageProps }) {
       <meta name="description" content="18-year-old CS and Business student building software applications using new technologies. Currently open for exciting internships for the summer of 2023."/>
       <meta name="keywords" content="software, developer, web development, AI, OpenAI, NextJS, TailwindCSS, Supabase, ReactJS, love letter, book club"/>
       </Head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-6L79FHE18Q"></Script>
+      <Script id="google-analytics">
+        {`
 
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6L79FHE18Q');
+        `}
+      </Script>
   <Component {...pageProps} />
     </>
     )
