e466eb6700dc083c03183c283d84a322dd779fae
Yajat Gulati <yajatgulati@Yajats-MacBook-Air.local>
2023-08-21 19:29:49 +0800

    updated after summer of 2023
    Signed-off-by: Yajat Gulati <yajatgulati@Yajats-MacBook-Air.local>

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import profile_pic from "../../public/profile_pic.png";
 import ProjectCard from "./components/ProjectCard";
 import LinkArrow from "./components/LinkArrow";
 import Header from "./components/Header";
+import ExperienceCard from "./components/ExperienceCard";
 export default function Home() {
   return (
     <div className="z-0 px-5 sm:px-24 xl:px-72 ">
@@ -37,32 +38,69 @@ export default function Home() {
           </div>
           <div className="relative p-10 mt-10 text-lg bg-green-300 rounded-xl ">
             <p>
-              I am currently collaborating with Dr. Fan and Prof. Zhu from NTU
-              on a research project that utilizes AI for evaluating
-              inter-disciplinary essays. Additionally, I am developing a web
-              application for a course called CC0001, aiming to enhance the
-              writing experience by providing students with efficient access to
-              essential insights. Whether you are interested in project
-              collaboration or simply want to have a chat, feel free to reach
-              out to me 👍
+              I've just started my second year at NTU, and I've taken on a whole
+              new set of responsibilities. I'll be undertaking the undergraduate
+              research program, here at NTU to work on sustanability recommender
+              systems with Alibaba-NTU Joint Research Institute (wow that's so
+              many words). But I'm also looking forward to meeting like minded
+              people through my positions as the Hackers for Charity Chairperson
+              at the NTU Open Source Society. Hopefully, my building ventures
+              will continue, I already have a couple ideas in mind. I'm nervous
+              and excited.
             </p>
             <span className="absolute text-sm right-10 bottom-30 text-slate-500">
-              Last updated: 21 June 2023
+              Last updated: 21 Aug 2023
             </span>
             <div className="flex flex-row gap-4">
               <a
                 href="mailto:yajatgulati01@gmail.com"
-                className="p-2 mt-10 text-lg text-white bg-black rounded-lg">
+                className="p-2 mt-10 text-lg text-white bg-black rounded-lg"
+              >
                 Get in Touch
               </a>
               <Link
                 href="https://docs.google.com/document/d/e/2PACX-1vR3vL03Y2CoiUujMSyWJpZCQH3y-iHgmKbfhnrcrH1mM1dDoN8MA2ROzHihmaQcdBVjJ6zaGWSmdNzx/pub"
-                className="mt-10 p-2 flex  flex-row items-center justify-start gap-0.5 transition-all hover:text-zinc-700 hover:gap-1 active:text-green-300 text-zinc-600 font-medium text-lg">
+                className="mt-10 p-2 flex  flex-row items-center justify-start gap-0.5 transition-all hover:text-zinc-700 hover:gap-1 active:text-green-300 text-zinc-600 font-medium text-lg"
+              >
                 Resume
                 <div className="w-5 h-5">
                   <LinkArrow />
                 </div>
               </Link>
+            </div>
+          </div>
+          <div className="mt-10">
+            <h1 className="text-2xl">work experience</h1>
+            <div className="flex flex-col gap-3 mt-10">
+              <ExperienceCard
+                position="Web Developer"
+                company="School of Humanities, Nanyang Technological University"
+                description="Collaborated with Prof Joanne Chia, Anglea Frattorla, and a talented UI/UX design team from NTU's School of Art, Design, and Media. Our collective aim was to develop a state-of-the-art web application for CC001: Inquiry and Communication in an Interdisciplinary World. The centerpiece of this application is an AI-feedback mechanism designed to guide students in refining their writing prowess."
+                techStack={[
+                  "MongoDB",
+                  "Node.js",
+                  "React.js",
+                  "Microsoft Azure",
+                  "Github Actions",
+                ]}
+                liveLink="https://cc001-webapp.com" // Add the actual link
+                githubLink="https://github.com/sheldor07/cc001-webapp" // Add the actual GitHub repo link
+              />
+
+              <ExperienceCard
+                position="Student Assistant"
+                company="National Institute of Education, Singapore"
+                description="I had the unique opportunity to join hands with Dr. Fan and Asst Prof Gaoxia, spearheading a project to automate an intricate framework. The objective? Systematic evaluation of interdisciplinary essays. The journey enriched my understanding of neural networks, specifically transformers. We leveraged models from huggingface to gauge metrics established by the Professors."
+                techStack={[
+                  "Python for AI",
+                  "MongoDB",
+                  "Node.js",
+                  "React.js",
+                  "Microsoft Azure",
+                  "Github Actions",
+                ]}
+                githubLink="https://github.com/sheldor07/essay-evaluator-research-assistantship" // Add the actual GitHub repo link
+              />
             </div>
           </div>
           <div name="projects" className="mt-10">
@@ -92,7 +130,8 @@ export default function Home() {
               <p className="text-xl text-slate-800"> LinkedIn </p>
               <a
                 href="https://www.linkedin.com/in/yajatgulati"
-                className="flex flex-row items-center justify-start gap-0.5 transition-all hover:text-zinc-700 hover:gap-1 active:text-green-300 text-zinc-600 font-medium text-lg">
+                className="flex flex-row items-center justify-start gap-0.5 transition-all hover:text-zinc-700 hover:gap-1 active:text-green-300 text-zinc-600 font-medium text-lg"
+              >
                 linkedin.com/in/yajatgulati{" "}
                 <div className="w-5 h-5">
                   <LinkArrow />
@@ -103,7 +142,8 @@ export default function Home() {
               <p className="text-xl text-slate-800"> Twitter </p>
               <a
                 href="https://twitter.com/GulatiYajat"
-                className="flex flex-row items-center justify-start gap-0.5 transition-all hover:text-zinc-700 hover:gap-1 active:text-green-300 text-zinc-600 font-medium text-lg">
+                className="flex flex-row items-center justify-start gap-0.5 transition-all hover:text-zinc-700 hover:gap-1 active:text-green-300 text-zinc-600 font-medium text-lg"
+              >
                 @GulatiYajat{" "}
                 <div className="w-5 h-5">
                   <LinkArrow />
@@ -114,7 +154,8 @@ export default function Home() {
               <p className="text-xl text-slate-800"> Github </p>
               <a
                 href="https://github.com/sheldor07"
-                className="flex flex-row items-center justify-start gap-0.5 transition-all hover:text-zinc-700 hover:gap-1 active:text-green-300 text-zinc-600 font-medium text-lg">
+                className="flex flex-row items-center justify-start gap-0.5 transition-all hover:text-zinc-700 hover:gap-1 active:text-green-300 text-zinc-600 font-medium text-lg"
+              >
                 @sheldor07
                 <div className="w-5 h-5">
                   <LinkArrow />
